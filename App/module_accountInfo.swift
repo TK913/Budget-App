886a706266e83c8951611c46b6d5dd9d61020a78
niyatisan710 <85580692+niyatisan710@users.noreply.github.com>
2023-11-15 15:44:18 -0800

    Update module_accountInfo.swift

--- App/module_accountInfo.swift
+++ App/module_accountInfo.swift
@@ -1,91 +1,55 @@
 import SwiftUI
-import SwiftData
 
 
 
 struct ContentView: View {
-    @State private var firstName = ""
-    @State private var lastName = ""
-    @State private var approxAnnualIncome = 0
-    @State private var username = ""
-    @State private var password = ""
+    @State private var firstName = "Niyati"
+    @State private var lastName = "Sanghrajka"
+    @State private var username = "niyatisan"
+    @State private var password = "password1"
     @State private var newPassword = ""
     @State private var newPasswordConfirm = ""
+    @State private var alert = false
 
-    func newPasswordConfirmation(newPassword: String, newPasswordConfirm: String, oldPassword: String) -> Bool{
-        //function should also check if oldPassword matches the password in the database
-        return newPassword == newPasswordConfirm
-    }
-
-    func updateBasics(){
-    //updates first name, last name, and income in database
-    }
-
-    func updateLoginInfo(){
-    //updates username and password in database
-        if(newPasswordConfirmation(newPassword, newPasswordConfirm, password)){
-
-        }
-        else{
-
-        }
-    }
-    
     var body: some View {
-        NavigationView{
-            ZStack {
-                Color.white
-                    .ignoresSafeArea()
-                VStack{
-                    Text("Account Information")
-                        .font(.largeTitle)
-                        .padding()
-                    Text("Basic Information")
-                        .font(.title)
-                        .padding()
-                    Text("First Name:")
-                        .font(.body)
-                        .padding()
-                    TextField("First Name", text: $firstName)
-                    Text("Last Name:")
-                        .font(.body)
-                        .padding()
-                    TextField("Last Name", text: $lastName)
-                    Text("Annual Income (approx)")
-                        .font(.body)
-                        .padding()
-                    TextField("Income", text: $approxAnnualIncome)
-                    Button("Update"){
-                        updateBasics()
+        Form {
+            Section(header: Text("Personal Information")) {
+                TextField("First Name", text: $firstName)
+                TextField("Last Name", text: $lastName)
+                TextField("Username", text: $username)
+            }
+
+            Section(header: Text("Password")) {
+                SecureField("Current Password", text: $password)
+                SecureField("New Password", text: $newPassword)
+                SecureField("Confirm New Password", text: $confirmNewPassword)
+            }
+
+            Section {
+                Button(action: {
+                    // Handle the button action
+                    if newPassword == confirmNewPassword {
+                        password = newPassword
+                        print("Changes saved successfully!")
+                    } else {
+                        showingAlert = true
                     }
-                    Text("Change Username/Password")
-                        .font(.title)
-                        .padding()
-                    Text("Username")
-                        .font(.body)
-                        .padding()
-                    TextField("Username", text: $username)
-                    Text("New Password")
-                        .font(.body)
-                        .padding()
-                    TextField("New Password", text: $newPassword)
-                    Text("Confirm New Password")
-                        .font(.body)
-                        .padding()
-                    TextField("Confirm Password", text: $newPasswordConfirm)
-                    Text("Old Password")
-                        .font(.body)
-                        .padding()
-                    TextField("Old Password", text: $password)
+                }) {
+                    Text("Save Changes")
                 }
             }
-            .navigationBarHidden(true)
+        }
+        .navigationTitle("Account Info")
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Error"), message: Text("New password and Confirm Password do not match."), dismissButton: .default(Text("OK")))
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct AccountInfoView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            AccountInfoView()
+        }
     }
 }
